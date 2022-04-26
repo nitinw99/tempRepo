da14bd5fe3aa2949db2b9e83aef171f619cbfc45
nitinw <nitinwankvani@gmail.com>
2022-04-26 17:07:14 +0530

    changes made to NewProgram.java file

--- NewProgram.java
+++ NewProgram.java
@@ -2,5 +2,6 @@ public class NewProgram {
     public static void main(String[] args) {
         System.out.println("helo");
         System.out.println("helo NEW");
+	System.out.println("helo3964346937");
     }
 }
